cc2fd02b4e3a2103f22786adc655227ca203b13d
wdrefubc <73660886+wdrefubc@users.noreply.github.com>
2022-04-08 11:17:49 -0700

    updates to js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import Mirador from "mirador/dist/mirador.min.js";
 Mirador.viewer({
   id: "app",
   window: {
-    views: [{ key: "scroll" }, { key: "gallery" }]
+    views: [{ key: "scroll" }, { key: "gallery" }, {key: "book"}]
   },
   windows: [
     {
-      manifestId: "https://purl.stanford.edu/zy841zs4569/iiif/manifest"
+      "loadedManifest": "https://iiif.lib.harvard.edu/manifests/drs:48309543",
+      "canvasIndex": 2,
+      "thumbnailNavigationPosition": 'far-bottom'
     }
-  ]
+  ],
+
 });
